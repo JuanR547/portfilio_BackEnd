71bb1963c2af0af41b62ec6cf3a9a3c2f35b6ba8
JuanR547 <95152166+JuanR547@users.noreply.github.com>
2023-05-14 16:23:56 -0300

    uwwu

---
+++ src/main/java/com/portfolioJR/jr/Service/ImpPersonaService.java
@@ -0,0 +1,36 @@
+package com.portfolioJR.jr.Service;
+
+import com.portfolioJR.jr.Entity.Persona;
+import com.portfolioJR.jr.Interface.IPersonaService;
+import com.portfolioJR.jr.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona  = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
