8a40a294416da6ed593ed00d0f7a3da0e79bae59
JuanR547 <95152166+JuanR547@users.noreply.github.com>
2023-05-14 12:32:02 -0300

    backend

---
+++ src/test/java/com/portfolioJuanReynoso/jr/JrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioJuanReynoso.jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
